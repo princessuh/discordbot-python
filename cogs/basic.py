056d040d5b24c8ed8671ba4215955ecffe3d1b45
VINA <sunn1estmay@gmail.com>
2025-07-19 13:00:15 +0900

    fix: 자연어 응답 파트 오류 수정

--- cogs/basic.py
+++ cogs/basic.py
@@ -77,5 +77,7 @@ class Basic(commands.Cog):
                 await message.channel.send("저요? 귀여운 애인 생각하고 있었죠!")
                 return
 
+        await self.bot.process_commands(message)
+
 async def setup(bot):
     await bot.add_cog(Basic(bot))
